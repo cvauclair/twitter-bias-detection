f31043a90711fec05c7e1db28682542529f1efb3
Karine Mellata <karine.mellata@mail.mcgill.ca>
2020-04-28 17:12:34 -0400

    .

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,47 +1,40 @@
+import datetime as dt
+import argparse
+from pprint import pprint
+
 import yaml
-from datetime import datetime
 from scraper import Scraper
 import json
 import pymysql
 from rds_controller import RDSController
+from sentiment.model import BERTWrapper
 from .topic_analysis.topic_analysis_controller import TopicAnalysisController
 
-
 class Pipeline:
-    def __init__(self):
-        self.scraper_configs = self.get_scraper_configs()
-        self.lda_configs = self.get_lda_configs()
+    def __init__(self, config_path):
+        # Load config
+        with open(config_path, 'r') as yaml_stream:
+            self.config = yaml.safe_load(yaml_stream)
+
         self.filename = self.build_filename()
 
-        # Scraper
-        self.scraper = Scraper(num_pages=self.scraper_configs['num_pages'],
-                               include_retweets=self.scraper_configs['include_retweets'],
-                               checkpoint=self.scraper_configs['checkpoint'],
-                               oldest_date=self.scraper_configs['oldest_date'],
-                               output_filename=self.filename)
-
-        # RDS Controller
+        # Init RDS Controller
+        print(f"[{dt.datetime.now()}] Connecting to RDS database")
         self.rds_controller = RDSController()
 
-    @staticmethod
-    def get_scraper_configs():
-        """
-        Get scraper configs from YAML file
-        :return: configs as a dict
-        """
-        with open('config.yaml', 'r') as yaml_stream:
-            configs = yaml.safe_load(yaml_stream)
-        return configs['scraper_configs']
+        # Init scraper
+        print(f"[{dt.datetime.now()}] Initializing scraper")
+        self.scraper = Scraper(**self.config['scraper_config'], output_filename=self.filename)
 
-    @staticmethod
-    def get_lda_configs():
-        """
-        Get scraper configs from YAML file
-        :return: configs as a dict
-        """
-        with open('config.yaml', 'r') as yaml_stream:
-            configs = yaml.safe_load(yaml_stream)
-        return configs['lda_configs']
+        # Init BERT wraper
+        print(f"[{dt.datetime.now()}] Initializing BERT sentiment model")
+        self.bert_wrapper = BERTWrapper(**self.config['sentiment_config'])
+
+        # Init LDA
+        # TODO
+
+        # Init Bias Inference
+        # TODO
 
     # This needs to change because eventually we will have more than
     # one user per file
@@ -51,8 +44,8 @@ class Pipeline:
         :param twitter_username: user who's tweets have been scraped
         :return: formatted filename
         """
-        current_date = datetime.now().strftime("%m_%d_%Y")
-        return "{}_{}".format('scraped_tweets', current_date)
+        current_date = dt.datetime.now().strftime("%m_%d_%Y")
+        return f"scraped_tweets_{current_date}"
 
     @staticmethod
     def read_json_file(file):
@@ -79,23 +72,23 @@ class Pipeline:
             with open(self.filename, 'w') as f:
                 json.dump(tweets, f, indent=4)
 
-    def run_pipeline(self):
+    def run_pipeline(self, accounts, recompute=False):
         # ----------------------------------------------
         # STAGE 1
         # Scraping of tweets
         # ----------------------------------------------
 
-        accounts = self.read_json_file(self.scraper_configs['accounts_file'])
-
-        all_tweets = {}
+        print(f"[{dt.datetime.now()}] Scraping tweets")
+        scraped_tweets = []
         for u in accounts:
             # Check if user exists. If not, create user.
-            user_id = self.scraper.extract_user_id(u['username'], None)
+            username = u['username'] if type(u) == dict else u
+            user_id = self.scraper.extract_user_id(username, None)
             user = self.rds_controller.get_user(user_id)
 
             if len(user) == 0:
                 try:
-                    user_profile = self.scraper.extract_user_profile(self.scraper, u['username'])
+                    user_profile = self.scraper.extract_user_profile(self.scraper, username)
                     self.rds_controller.create_user(
                         user_profile['id'],
                         user_profile['username'],
@@ -111,39 +104,75 @@ class Pipeline:
                     print(format(err))
                     continue
 
-            user_tweets = self.scraper.scrape_tweets(u['username'])
-
+            user_tweets = self.scraper.scrape_tweets(username)
             if user_tweets:
-                all_tweets[u['username']] = user_tweets
+                scraped_tweets.extend(self.scraper.scrape_tweets(username))
 
-        self.write_output(tweets=all_tweets)
+        self.write_output(tweets=scraped_tweets)
+        # scraped_tweets = self.read_json_file(self.filename)
+
+        # Update DB
+        print(f"[{dt.datetime.now()}] Uploading scraped tweets to RDS database")
+        for tweet in scraped_tweets:
+            try:
+                self.rds_controller.create_tweet(**tweet)
+            except:
+                pass
 
         # ----------------------------------------------
         # STAGE 2
         # BERT
         # ----------------------------------------------
+        print(f"[{dt.datetime.now()}] Computing tweets sentiment")
+
+        if recompute:
+            all_tweets = self.rds_controller.get_all_tweets()
+        else:
+            all_tweets = scraped_tweets
+
+        print(f"[DEBUG] num tweets to analyze sentiment: {len(all_tweets)}")
+
+        try:
+            tweet_sent = self.bert_wrapper.predict([t['content'] for t in all_tweets])
+        except Exception as e:
+            print(f"[ERROR] Could not predict tweet sentiment, no sentiment computed")
+            print(e)
+            tweet_sent = []
+
+        for i, tweet in enumerate(all_tweets):
+            tweet['sentiment'] = tweet_sent[i]['sentiment']
+
+        pprint(all_tweets)
+
+        # Update tweet db
+        print(f"[{dt.datetime.now()}] Updating tweets sentiment in database")
+        for tweet in all_tweets:
+            self.rds_controller.set_tweet_sentiment(
+                tweet_id=tweet['tweet_id'],
+                sentiment=tweet['sentiment']
+            )
 
 
         # ----------------------------------------------
         # STAGE 3
         # LDA
         # ----------------------------------------------
+
         lda_controller = TopicAnalysisController()
 
-        #models = self.read_json_file(self.lda_configs['accounts_file'])
         models = ['realDonaldTrump']
         for u in accounts:
             if u['username'] in models:
-                user_tweets = all_tweets[u['username']]
-                user_tweets_content = [user_tweets[i][1] for i in range(len(user_tweets))]
-                user_tweets_id = [user_tweets[i][0] for i in range(len(user_tweets))]
+                user_tweets = [t for t in scraped_tweets if i['username'] == u['username']]
+                user_tweets_content = [t['content'] for t in user_tweets]
+                user_tweets_id = [t['id'] for t in user_tweets]
+
                 if user_tweets:
-                    tweets_topics = lda_controller.compute_topic_id_for_tweets(tweets= user_tweets_content, username= u['username'])
+                    # This happens per user
+                    tweets_topics = lda_controller.compute_topic_id_for_tweets(tweets=user_tweets_content, username= u['username'])
 
                 for id, topic in user_tweets_id, tweets_topics:
                     RDSController.set_tweet_topic(tweet_id= id, topic_id= topic)
-
-
 
         # ----------------------------------------------
         # STAGE 4
